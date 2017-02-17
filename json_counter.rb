5bcdbb8a012b5e7b9578c87f9b6699bea7dd433d
Anas Arifi <anas.arifi@gmail.com>
2017-02-17 11:39:45 +0100

    edit json counter

--- json_counter.rb
+++ json_counter.rb
@@ -3,4 +3,13 @@ require 'json'
 f = File.open(ARGV.first)
 hash = JSON.parse(File.read(f))
 
-puts hash['response']['venues'].count
+#for foursquare
+#puts hash['results'].count
+
+results = hash['results']
+
+#print results.count
+results.each {|result| print result['name'] + "\n"}
+
+#for yelp
+#puts hash['businesses'].count
